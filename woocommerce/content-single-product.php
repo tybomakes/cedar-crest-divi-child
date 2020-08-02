81f080dbd81f151029be566ed0a1b75137359c49
Tybo <jtyborski@gmail.com>
2020-08-02 17:33:01 -0500

    Added additional sizes, etc.

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -97,7 +97,7 @@ if ( post_password_required() ) {
 	return;
 }
 ?>
-test
+
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
 	<div class="banner">
 		<div>
@@ -117,32 +117,246 @@ test
 			WISC LOGO
 		</div>
 	</div>
-	<div class="size-tabs">
-		<div>
-			<h2>Find <?php echo get_the_title(); ?> near you.</h2>
-			<p>Available in pints and tubs.</p>
+	<div class="size-section">
+		<div class="size-text">
+			<h2>Choose a Size</h2>
 		</div>
-		<div>
-			<a href="#tub" class="size-activator active">
-				<img src="https://placehold.it/250" />
-			</a>
-		</div>
-		<div>
-			<a href="#pint" class="size-activator">
-				<img src="https://placehold.it/250" />
-			</a>
-		</div>
-		<div>
-			<a href="#cup" class="size-activator">
-				<img src="https://placehold.it/250" />
-			</a>
+		<div class="svgbtns">
+			<div>
+				<a href="#3gal" class="size-activator active">
+					<img src="https://placehold.it/150" />
+				</a>
+			</div>
+			<div>
+				<a href="#scround" class="size-activator">
+					<img src="https://placehold.it/150" />
+				</a>
+			</div>
+			<div>
+				<a href="#pint" class="size-activator">
+					<img src="https://placehold.it/150" />
+				</a>
+			</div>
+			<div>
+				<a href="#cup" class="size-activator">
+					<img src="https://placehold.it/150" />
+				</a>
+			</div>
 		</div>
 	</div>
+
 	<div class="product-info">
 		<div>
-			<section data-size="tub" class="performance-facts">
+			<section data-size="3gal" class="performance-facts">
 				<header class="performance-facts__header">
-					<h1 class="performance-facts__title">Nutrition Facts - TUB</h1>
+					<h1 class="performance-facts__title">Nutrition Facts - 3Gal</h1>
+					<p>Serving Size 1/2 cup (about 82g)
+						<p>Serving Per Container 8</p>
+				</header>
+				<table class="performance-facts__table">
+					<thead>
+						<tr>
+							<th colspan="3" class="small-info">
+								Amount Per Serving
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th colspan="2">
+								<b>Calories</b>
+								200
+							</th>
+							<td>
+								Calories from Fat
+								130
+							</td>
+						</tr>
+						<tr class="thick-row">
+							<td colspan="3" class="small-info">
+								<b>% Daily Value*</b>
+							</td>
+						</tr>
+						<tr>
+							<th colspan="2">
+								<b>Total Fat</b>
+								14g
+							</th>
+							<td>
+								<b>22%</b>
+							</td>
+						</tr>
+						<tr>
+							<td class="blank-cell">
+							</td>
+							<th>
+								Saturated Fat
+								9g
+							</th>
+							<td>
+								<b>22%</b>
+							</td>
+						</tr>
+						<tr>
+							<td class="blank-cell">
+							</td>
+							<th>
+								Trans Fat
+								0g
+							</th>
+							<td>
+							</td>
+						</tr>
+						<tr>
+							<th colspan="2">
+								<b>Cholesterol</b>
+								55mg
+							</th>
+							<td>
+								<b>18%</b>
+							</td>
+						</tr>
+						<tr>
+							<th colspan="2">
+								<b>Sodium</b>
+								40mg
+							</th>
+							<td>
+								<b>2%</b>
+							</td>
+						</tr>
+						<tr>
+							<th colspan="2">
+								<b>Total Carbohydrate</b>
+								17g
+							</th>
+							<td>
+								<b>6%</b>
+							</td>
+						</tr>
+						<tr>
+							<td class="blank-cell">
+							</td>
+							<th>
+								Dietary Fiber
+								1g
+							</th>
+							<td>
+								<b>4%</b>
+							</td>
+						</tr>
+						<tr>
+							<td class="blank-cell">
+							</td>
+							<th>
+								Sugars
+								14g
+							</th>
+							<td>
+							</td>
+						</tr>
+						<tr class="thick-end">
+							<th colspan="2">
+								<b>Protein</b>
+								3g
+							</th>
+							<td>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+
+				<table class="performance-facts__table--grid">
+					<tbody>
+						<tr>
+							<td colspan="2">
+								Vitamin A
+								10%
+							</td>
+							<td>
+								Vitamin C
+								0%
+							</td>
+						</tr>
+						<tr class="thin-end">
+							<td colspan="2">
+								Calcium
+								10%
+							</td>
+							<td>
+								Iron
+								6%
+							</td>
+						</tr>
+					</tbody>
+				</table>
+
+				<p class="small-info">* Percent Daily Values are based on a 2,000 calorie diet. Your daily values may be higher or lower depending on your calorie needs:</p>
+
+				<table class="performance-facts__table--small small-info">
+					<thead>
+						<tr>
+							<td colspan="2"></td>
+							<th>Calories:</th>
+							<th>2,000</th>
+							<th>2,500</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th colspan="2">Total Fat</th>
+							<td>Less than</td>
+							<td>65g</td>
+							<td>80g</td>
+						</tr>
+						<tr>
+							<td class="blank-cell"></td>
+							<th>Saturated Fat</th>
+							<td>Less than</td>
+							<td>20g</td>
+							<td>25g</td>
+						</tr>
+						<tr>
+							<th colspan="2">Cholesterol</th>
+							<td>Less than</td>
+							<td>300mg</td>
+							<td>300 mg</td>
+						</tr>
+						<tr>
+							<th colspan="2">Sodium</th>
+							<td>Less than</td>
+							<td>2,400mg</td>
+							<td>2,400mg</td>
+						</tr>
+						<tr>
+							<th colspan="3">Total Carbohydrate</th>
+							<td>300g</td>
+							<td>375g</td>
+						</tr>
+						<tr>
+							<td class="blank-cell"></td>
+							<th colspan="2">Dietary Fiber</th>
+							<td>25g</td>
+							<td>30g</td>
+						</tr>
+					</tbody>
+				</table>
+
+				<p class="small-info">
+					Calories per gram:
+				</p>
+				<p class="small-info text-center">
+					Fat 9
+					&bull;
+					Carbohydrate 4
+					&bull;
+					Protein 4
+				</p>
+
+			</section>
+			<section data-size="scround" class="performance-facts hide">
+				<header class="performance-facts__header">
+					<h1 class="performance-facts__title">Nutrition Facts - scround</h1>
 					<p>Serving Size 1/2 cup (about 82g)
 						<p>Serving Per Container 8</p>
 				</header>
@@ -349,7 +563,7 @@ test
 			</section>
 			<section data-size="pint" class="performance-facts hide">
 				<header class="performance-facts__header">
-					<h1 class="performance-facts__title">Nutrition Facts - PINT</h1>
+					<h1 class="performance-facts__title">Nutrition Facts - pint</h1>
 					<p>Serving Size 1/2 cup (about 82g)
 						<p>Serving Per Container 8</p>
 				</header>
@@ -762,7 +976,7 @@ test
 
 			</section>
 		</div>
-		<div>
+		<div class="ingredients">
 			<h3>Ingredients</h3>
 			<p><?php the_field('ingredients'); ?></p>
 			<h3>Allergy Information</h3>
